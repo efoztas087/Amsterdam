606482dbea9ef6ca57a7a8de0578dc9c4b6a3e5e
efe <efe.oztas1@icloud.com>
2025-12-14 22:07:07 +0100

    fx problems

--- amsterdam-cms/prisma.config.ts
+++ amsterdam-cms/prisma.config.ts
@@ -3,12 +3,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
 
-  datasources: {
-    db: {
-      url: "file:./prisma/dev.db",
-    },
-  },
-
   migrations: {
     path: "prisma/migrations",
   },
