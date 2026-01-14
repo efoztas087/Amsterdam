8cda773c73e01c1ed3f55096647692e8dc1b5fc7
efe <efe.oztas1@icloud.com>
2026-01-15 00:06:58 +0100

    admin publicaties gevoegd

---
+++ amsterdam-cms/src/app/(admin)/admin/publicaties/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PublicatiesAdminPage() {
+  return (
+    <main className="admin-content">
+      <h1>Publicaties beheren</h1>
+      <p>Hier kun je straks publicaties toevoegen en bewerken.</p>
+    </main>
+  );
+}
